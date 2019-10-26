7379a62ed4b021160c79a37055ebd6431d0885be
Marc Hauschildt <marc.hauschildt@gmail.com>
2019-10-26 13:57:33 -0500

    Functions simplify my code and give it more abilities

--- app.js
+++ app.js
@@ -16,18 +16,49 @@ function draw() {
 }
 
 function day1AM() {
-    console.log("Hello World");
+    // console.log("Hello World");
     var fullName = "Marc Hauschildt";
     var age = 38;
     var weight = 192.5;
-    var heightInInches = 75;
+    var heightInInches = 68;
     var address = "123 Main St.";
     var middleName;
-    console.log(middleName);
-    console.log(typeof(weight));
+    // console.log(middleName);
+    // console.log(typeof(weight));
     var likesPizza = true;
     var hasTattoos = false;
-    console.log(typeof(likesPizza));
+    // console.log(typeof(likesPizza));
+    console.log(age);
+    age = 39;
+    console.log(age);
+    age = age + 1;
+    console.log(age);
+    age++;
+    age += 1;
+    age *= 5;
+    age = age * 5;
+    age /= 10;
+    console.log(age);
+
+    console.log(fullName + " is " + age + " years old.");
+    console.log("Do you like pizza? " + likesPizza);
+    var feet = Math.floor(heightInInches / 12);
+    var inches = heightInInches % 12;
+    console.log(fullName + " is " + feet + " foot " + inches +" inches tall");
+
+    print("Hello World");
+    var convertedTemp = f2c(32);
+    print(convertedTemp);
 }
 
-day1AM();
+day1AM();  
+
+function print(input) {
+    console.log(input);
+}
+
+// Converts Fahrenheit to Celsius
+// Input: temperature in fahrenheit
+function f2c(t) {
+    return((t - 32) * 5 / 9);
+}

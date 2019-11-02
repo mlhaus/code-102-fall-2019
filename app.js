df3cdea748e1bf66313771f22a49faf614af0b5c
Marc Hauschildt <marc.hauschildt@gmail.com>
2019-11-02 11:11:23 -0500

    I learned arrays and for loops

--- app.js
+++ app.js
@@ -106,18 +106,48 @@ function day2AM() {
     }
 
 
-    var sum = 0;
-    var count = 0;
-    while(true) {
-        var input = parseInt(prompt('Enter a number. Type 0 to end. '));
-        if(input === 0) {
-            break;
-        } else {
-            sum += input;
-            count++;
-        }
+    // var sum = 0;
+    // var count = 0;
+    // while(true) {
+    //     var input = parseInt(prompt('Enter a number. Type 0 to end. '));
+    //     if(input === 0) {
+    //         break;
+    //     } else {
+    //         sum += input;
+    //         count++;
+    //     }
+    // }
+    // console.log('The average of your numbers is ' + sum/count);
+
+    for(var count = 1; count <= 100; count++) {
+        print(count);
     }
-    console.log('The average of your numbers is ' + sum/count);
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    print(months[0]);
+    print(months[1]);
+    print(months[2]);
+    print(months[3]);
+    print(months[4]);
+    // i stands for iteration
+    for(var i = 0; i < months.length; i++) {
+        print(months[i]);
+    }
+
+    var states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+    states.forEach(function(state) {
+        print(state);
+    });
+
+    states.forEach(function (state){
+        if(state === "Iowa" || state == "Hawaii"){
+            print(state + " is cool");
+        } else {
+            print(state + " sucks!");
+        }
+    });
+
 
 }
 
